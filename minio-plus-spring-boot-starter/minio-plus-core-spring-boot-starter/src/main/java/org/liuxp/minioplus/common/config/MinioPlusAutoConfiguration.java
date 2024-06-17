8ba0a37a2d3662989cbf0ae53e046dacfd53b40e
刘小平 <contact@liuxp.me>
2024-06-17 14:41:44 +0800

    增加minio-plus-core-spring-boot-starter和minio-plus-application-schedule

---
+++ minio-plus-spring-boot-starter/minio-plus-core-spring-boot-starter/src/main/java/org/liuxp/minioplus/common/config/MinioPlusAutoConfiguration.java
@@ -0,0 +1,14 @@
+package org.liuxp.minioplus.common.config;
+
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * MinioPlusAutoConfiguration
+ *
+ * @author contact@liuxp.me
+ * @since 2024/06/11
+ */
+@ComponentScan("org.liuxp.minioplus")
+public class MinioPlusAutoConfiguration {
+
+}
